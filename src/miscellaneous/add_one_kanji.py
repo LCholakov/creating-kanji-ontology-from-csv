caf5cc12032c5908bb2e9bebb59d8e96320211f6
Lyubomir Cholakov <39132342+LCholakov@users.noreply.github.com>
2026-05-28 10:01:01 +0300

    rename namespace

--- src/miscellaneous/add_one_kanji.py
+++ src/miscellaneous/add_one_kanji.py
@@ -1,18 +1,18 @@
 from rdflib import Graph, Namespace, RDF, RDFS, Literal
 from rdflib.namespace import OWL
 
-kanji_ns  = Namespace("https://example.org/kanji#")
+JK  = Namespace("https://example.org/joyokanji#")
 
 graph = Graph()
-graph.bind("kanji", kanji_ns)
+graph.bind("jk", JK)
 graph.bind("owl", OWL)
 graph.bind("rdfs", RDFS)
 
-graph.add((kanji_ns.KanjiCharacter, RDF.type, OWL.Class))
+graph.add((JK.KanjiCharacter, RDF.type, OWL.Class))
 
 # Create one instance: 愛
-kanji = kanji_ns["kanji_U611B"]
-graph.add((kanji, RDF.type, kanji_ns.KanjiCharacter))
+kanji = JK["kanji_U611B"]
+graph.add((kanji, RDF.type, JK.KanjiCharacter))
 graph.add((kanji, RDFS.label, Literal("愛")))
 
 graph.serialize("out/add_one_kanji.ttl", format="turtle")
